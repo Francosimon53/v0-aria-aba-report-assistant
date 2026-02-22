7a4ef9cc6c7d84cad2f8da9074ce3890f126ec67
Francosimon53 <francosimon@hotmail.com>
2026-02-22 10:52:15 -0500

    Revert "feat: enhance chat with Motor Brain assessment workflow"
    This reverts commit 5a74973eb4fddad3d678961677777c8f393e8117.

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,15 +1,13 @@
 "use client"
 
-import { ChatInterface } from "@/components/chat-interface"
+import { Dashboard } from "@/components/dashboard"
 import { Navbar } from "@/components/navbar"
 
 export default function ChatPage() {
   return (
-    <div className="flex flex-col h-screen">
+    <>
       <Navbar />
-      <div className="flex-1 overflow-hidden">
-        <ChatInterface />
-      </div>
-    </div>
+      <Dashboard />
+    </>
   )
 }
